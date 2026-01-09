6f99b82a5ff8019bc043ac0298e60f131aee022d
waleedtestt <waleedacademy111@gmail.com>
2026-01-10 00:30:51 +0300

    Create AdminPage.tsx

---
+++ AdminPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h2>Welcome to the Admin Panel</h2>
+      <p>Here you can manage your data.</p>
+    </div>
+  );
+};
+
+export default AdminPage;
